cc553961e124a2c164f73c665de624e3d1243475
Kevin Tse <kevin.tse@prezi.com>
2016-09-10 15:12:25 -0400

    Adding Markov Sentence Generation

--- question_similarity.py
+++ question_similarity.py
@@ -2,8 +2,20 @@ from collections import defaultdict
 import logging, os, re
 
 from gensim import corpora, models, similarities
+import markovify
+import nltk
 
 logging.basicConfig(format='%(asctime)s : %(levelname)s : %(message)s', level=logging.INFO)
+
+class POSifiedText(markovify.NewlineText):
+    def word_split(self, sentence):
+        words = re.split(self.word_split_pattern, sentence)
+        words = [ "::".join(tag) for tag in nltk.pos_tag(words) ]
+        return words
+
+    def word_join(self, words):
+        sentence = " ".join(word.split("::")[0] for word in words)
+        return sentence
 
 def parse_interview(speeches_dir):
     question_answers = {}
@@ -65,7 +77,8 @@ def find_best_answer(texts, processed_question):
 
     sims = index[vec_lsi]
     sims = sorted(enumerate(sims), key=lambda item: -item[1])
-    return sims[0][0]
+
+    return sims
 
 def get_trump_answer(question):
     question_answers = parse_interview('speeches')
@@ -73,9 +86,32 @@ def get_trump_answer(question):
 
     _, processed_question = load_documents([question])
 
-    answer_index = find_best_answer(texts, processed_question[0])
-    key = "".join(texts[answer_index])
+    if processed_question:
+        similarity_scores = find_best_answer(texts, processed_question[0])
+        if similarity_scores[0][1] > 0:
+            answer_index = similarity_scores[0][0]
+            key = "".join(texts[answer_index])
+            return keyword_full_answer_mapping[key]
 
-    return keyword_full_answer_mapping[key]
+    return generate_trump_sentences(keyword_full_answer_mapping)
 
-print(get_trump_answer('What is the best thing about Obama'))
+def generate_trump_sentences(keyword_full_answer_mapping):
+    markov_text = []
+    for answer in keyword_full_answer_mapping.values():
+        markov_text.append(answer)
+
+    model_all = POSifiedText("\n".join(markov_text))
+
+    with open("speeches/famous_quotes.txt") as f:
+        quotes = f.read()
+
+    model_quotes = POSifiedText(quotes)
+    model_combo = markovify.combine([ model_quotes, model_all ], [ 3, 1 ])
+
+    generated_sentence = None
+    while generated_sentence is None:
+        generated_sentence = model_combo.make_short_sentence(140)
+    generated_sentence = " ".join(word.split("::")[0] for word in generated_sentence.split(" "))
+    return generated_sentence
+
+print(get_trump_answer('how are you'))
